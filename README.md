d648b84ae0c46c3cbefd8af2fb0619de8a20eee1
Steve Trewick <steve.trewick@icloud.com>
2025-01-07 18:23:14 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UVC
+
+A description of this package.
---
+++ Tests/UVCTests/UVCTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import UVC
+
+final class UVCTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(UVC().text, "Hello, World!")
+    }
+}
