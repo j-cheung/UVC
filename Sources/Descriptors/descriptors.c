7f82b4e1c1df7f2fe9b92b1d1cdd260810335011
Steve Trewick <steve.trewick@icloud.com>
2025-01-07 22:38:02 +0000

    easter egg

--- Sources/Descriptors/descriptors.c
+++ Sources/Descriptors/descriptors.c
@@ -3,13 +3,13 @@
 #include "include/descriptors.h"
 
 /*
-  this a fakey kind of bidging header, since you're interested.
+  this is a fakey kind of bridging header, since you're interested.
   note that we have a C dependency in our swift package!
  
   all the stuff we actualy want is in includes/descriptors.h
   and is all C structs. we have to use c structs becuase we have to pack them
   to get them filled from pointers, and swift (in the version I'm stuck on
-  at leats) still won't do that.
+  at least) still won't do that.
  
   anyway, if you want to know how that works have a look at
   
